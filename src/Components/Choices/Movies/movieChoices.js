e1edceb0ce03ce097587e0fde69c686f1526a5e4
Adam Saulters <adamsaulters@gmail.com>
2022-05-08 21:38:59 -0400

    still haven't gotten it to display some thumbnails... 2

--- src/Components/Choices/Movies/movieChoices.js
+++ src/Components/Choices/Movies/movieChoices.js
@@ -4,6 +4,8 @@ import bookmarkSVG from './assets/icon-bookmark-empty.svg';
 
 import classes from './movieChoices.module.css';
 
+import ran from './'
+
 const movieChoices = (props) => {
   return (
     <div>
@@ -14,10 +16,10 @@ const movieChoices = (props) => {
                       className={classes.movieChoiceDiv}
                       key={i}
                     >
-                      <div className={classes.movieThumbPic}>
-                        <img src={bookmarkSVG} />
-                        <img className={classes.movieImg} src={movie.thumbnail.regular.large} />
-                      </div>
+                                <div className={classes.movieThumbPic}> 
+                                    <img src={bookmarkSVG} className={classes.bookmarkSVG}  alt='bookmark img'/>
+                                    <img className={classes.recommendedMovieImg} src={movie.thumbnail.regular.large}/>
+                                </div>
                       <div className={classes.movieInfo}>
                          <p>{movie.year} * {movie.category} * {movie.rating} </p>
                          <h3 className={classes.movieTitleH4}>{movie.title}</h3> 
